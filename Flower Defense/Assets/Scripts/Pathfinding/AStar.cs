24bc4da46b8450bdb2babdfd5483b08806c60648
Gabor Tornyos <torgabor@gmail.com>
2023-10-28 12:06:40 +0200

    pathfinding initial

---
+++ Flower Defense/Assets/Scripts/Pathfinding/AStar.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Pathfinding
+{
+    public class Node : IEquatable<Node>
+    {
+        public Vector2Int pos;
+        public float gCost = 0;  // Cost from start to this node
+        public Node src;  // Parent node for path tracing
+
+        public bool Equals(Node other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return pos.Equals(other.pos);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Node)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return pos.GetHashCode();
+        }
+    }
+    public class AStar
+    {
+        List<Node> openSet = new ();
+        HashSet<Vector2Int> closedSet = new ();
+        private List<Vector2Int> neighbors = new List<Vector2Int>();
+        private Dictionary<Vector2Int, Node> nodes = new(); 
+        
+        public List<Vector2Int> AStarSearch(Vector2Int startPos, Vector2Int endPos, Pathfinder pathfinder)
+        {
+             float FCost(Node a)
+             {
+                 var hCost = HCost(a);
+                 return hCost + a.gCost;
+             }
+             float HCost(Node a)
+             {
+                 var hCost = GetDistance(a.pos, endPos);
+                 return hCost;
+             }
+
+             Node GetNode(Vector2Int pos) =>
+                 nodes.TryGetValue(pos, out var v) ? v : nodes[pos] = new Node() { pos = pos };
+            nodes.Clear();
+            openSet.Clear();
+            closedSet.Clear();
+            var startNode = GetNode(startPos);
+            openSet.Add(startNode);
+    
+            while (openSet.Count > 0)
+            {
+                // Find node with lowest fCost
+                Node currentNode = openSet[0];
+                for (int i = 1; i < openSet.Count; i++)
+                {
+                    if (FCost(openSet[i]) < FCost(currentNode))
+                    {
+                        currentNode = openSet[i];
+                    }
+                }
+
+                // Remove current from openSet and add to closedSet
+                openSet.Remove(currentNode);
+                closedSet.Add(currentNode.pos);
+
+                // Found the target
+                if (currentNode.pos == endPos)
+                {
+                    return RetracePath(startPos, currentNode);
+                }
+
+                // Check all neighbors
+                neighbors.Clear();
+                pathfinder.GetNeighbors(currentNode.pos, neighbors);
+                foreach (var nPos in neighbors)
+                {
+                    if (closedSet.Contains(nPos))
+                    {
+                        continue;
+                    }
+
+                    var neighbor = GetNode(nPos);
+                    float newGCost = currentNode.gCost + GetDistance(currentNode.pos, nPos);
+                    if (newGCost < neighbor.gCost || !openSet.Contains(neighbor))
+                    {
+                        Debug.Log($"updating node: {currentNode.pos.x},{currentNode.pos.y}");
+                        neighbor.gCost = newGCost;
+                        neighbor.src = currentNode;
+
+                        if (!openSet.Contains(neighbor))
+                        {
+                            openSet.Add(neighbor);
+                        }
+                    }
+                   
+                }
+            }
+
+            // Return null if no path is found
+            return null;
+        }
+
+        private static float GetDistance(Vector2Int a, Vector2Int b)
+        {
+            // Use basic manhattan distance
+            return Mathf.Abs(a.x - b.x) + Mathf.Abs(a.y - b.y);
+        }
+
+        public List<Vector2Int> RetracePath(Vector2Int startPos, Node endNode)
+        {
+            var path = new List<Vector2Int>();
+            Node currentNode = endNode;
+    
+            while (currentNode.pos != startPos)
+            {
+                path.Add(currentNode.pos);
+                currentNode = currentNode.src;
+            }
+            path.Add(startPos);
+            path.Reverse();
+            return path;
+        }
+    }
+}
---
+++ Flower Defense/Assets/Scripts/Pathfinding/Pathfinder.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Pathfinder : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+    public Tile startTile;
+    public Tile endTile;
+    public Tile wallTile;
+
+    BoundsInt bounds;
+    
+    private List<Vector2Int> path;
+    private AStar _aStar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+        // Debug.Log($"{bounds} count: ${allTiles.Length}");
+        // foreach (var(tile, pos) in IterateTiles())
+        // {
+        //     Debug.Log($"x:{pos.x} y:{pos.y} {tile.name}");
+        // }
+
+        _aStar = new AStar();
+        var (start, end) = FindStartAndEnd();
+         path =_aStar.AStarSearch(start, end, this);
+         if (path == null)
+         {
+             Debug.Log("no path found");
+         }
+         else
+         {
+             Debug.Log($"path is ${path.Count} long");
+         }
+         
+    }
+
+    private IEnumerable<(TileBase tile, Vector2Int pos)> IterateTiles()
+    {
+        for (int xx = bounds.xMin; xx <= bounds.xMax; xx++)
+        {
+            for (int yy = bounds.yMin; yy <= bounds.yMax; yy++)
+            {
+                var tile = tilemap.GetTile(new Vector3Int(xx, yy));
+                
+                if (tile != null)
+                {
+                    yield return (tile, new Vector2Int(xx, yy));
+                }
+            }
+        }
+    }
+
+    public void GetNeighbors(Vector2Int pos, List<Vector2Int> neighbors)
+    {
+        const bool skipDiag = true;
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                // Skip the node itself
+                if (dx == 0 && dy == 0)
+                    continue;
+
+                // Skip diagonal neighbors
+                if (skipDiag && (dx != 0 && dy != 0))
+                    continue;
+
+                int newX = pos.x + dx;
+                int newY = pos.y + dy;
+
+                if (InBounds(newX, newY))
+                {
+                    var tileBase = tilemap.GetTile(new Vector3Int(newX, newY));
+                    if (IsWalkable(tileBase))
+                    {
+                        neighbors.Add(new Vector2Int(newX, newY));
+                    }
+                }
+            }
+        }
+    }
+
+    public (Vector2Int start, Vector2Int end) FindStartAndEnd()
+    {
+        Vector2Int start = Vector2Int.zero;
+        Vector2Int end = Vector2Int.zero;
+        foreach (var (tile,pos) in IterateTiles())
+        {
+            if (tile == startTile)
+                start = pos;
+            if (tile == endTile)
+                end = pos;
+        }
+
+        return (start, end);
+    }
+
+    private bool IsWalkable(TileBase tile)
+    {
+        return tile != wallTile;
+    }
+
+    private bool InBounds(int newX, int newY)
+    {
+        return newX >= bounds.xMin && newX <= bounds.xMax && newY >= bounds.yMin && newY <= bounds.yMax;
+    }
+    void OnDrawGizmos()
+    {
+        if (path != null && path.Count > 1)
+        {
+            for (int i = 0; i < path.Count - 1; i++)
+            {
+                float t = (float)i / (path.Count - 1);
+                Gizmos.color = Color.Lerp(Color.green, Color.red, t);
+                
+                Vector3 startWorldPos = tilemap.GetCellCenterWorld((Vector3Int)path[i]);
+                Vector3 endWorldPos = tilemap.GetCellCenterWorld((Vector3Int)path[i + 1]);
+
+                Gizmos.DrawLine(startWorldPos, endWorldPos);
+            }
+        }
+    }
+}
