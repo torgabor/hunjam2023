ca5745894d842b6ea3d8a0cd6b75679b6e2fadee
Gabor Tornyos <torgabor@gmail.com>
2023-10-28 22:08:35 +0200

    add random offset

--- Flower Defense/Assets/Scripts/Pathfinding/CropStateManager.cs
+++ Flower Defense/Assets/Scripts/Pathfinding/CropStateManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = System.Random;
 
 
 public class CropStateManager : StateManager
@@ -11,12 +12,13 @@ public class CropStateManager : StateManager
         public Upgradeable Upgradeable;
         public Destroyable Destroyable;
     }
-    [HideInInspector]
-    public State[] Grid;
+
+    [HideInInspector] public State[] Grid;
     public int Width;
     public int Height;
     public GameObject grassPrefab;
     public Vector2 prefabSize;
+    public Vector2 prefabRandomOffset;
     public float[] MoveMulitplierByLevel;
 
     public void Start()
@@ -28,11 +30,16 @@ public class CropStateManager : StateManager
     {
         Grid = new State[Width * Height];
         int idx = 0;
+        var random = new Random();
         for (int xx = 0; xx < Width; xx++)
         {
-            for (int yy = 0; yy < Height; yy++,idx++)
+            for (int yy = 0; yy < Height; yy++, idx++)
             {
                 var pos = new Vector3(prefabSize.x * xx, prefabSize.y * yy);
+                var offset = new Vector3(
+                    ((float)random.NextDouble() - 0.5f) * prefabRandomOffset.x,
+                    ((float)random.NextDouble() - 0.5f) * prefabRandomOffset.y);
+                pos += offset;
                 var go = Instantiate(grassPrefab, transform);
                 go.transform.localPosition = pos;
                 var upgradeable = go.GetComponentInChildren<Upgradeable>();
