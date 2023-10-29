742e4da6ccbe2c5f9d9d924e7ebe0682b76ba99e
Gabor Tornyos <torgabor@gmail.com>
2023-10-29 16:49:16 +0100

    fix particle sorting

---
+++ Flower Defense/Assets/sortparticle.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sortparticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // var ps = GetComponent<ParticleSystem>() 
+        // ps.renderer.sortingLayerName = "Foreground";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
